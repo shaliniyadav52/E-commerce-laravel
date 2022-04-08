d5b2b2804b425e243a89f3c7709332466d035e1d
Shalini yadav <shalini.gyadav887@gmail.com>
2022-04-08 18:54:54 +0530

    Cart product Listing

--- E-Commerce/resources/views/header.blade.php
+++ E-Commerce/resources/views/header.blade.php
@@ -29,7 +29,7 @@ if(Session::has('user'))
           <button type="submit" class="btn btn-default">Search</button>
         </form>
         <ul class="nav navbar-nav navbar-right">
-       <li><a href="#">Cart({{$total}})</a>
+       <li><a href="/cartlist">Cart({{$total}})</a>
        </li>
        @if(Session::has('user'))
        <li class="dropdown">
